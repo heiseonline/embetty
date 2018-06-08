b149636ef76f483a3a727292bca38f8ec4ab02d8
pmb <pmb@heise.de>
2018-06-08 10:13:05 +0200

    fix webcomponentsjs usage

--- polyfills.js
+++ polyfills.js
@@ -5,5 +5,5 @@ try {
   eval(require('raw-loader!@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js'))
 } catch (error) {}
 
-require('@webcomponents/webcomponentsjs')
+require('@webcomponents/webcomponentsjs/webcomponents-bundle.js')
 require('intersection-observer')
