feb3a92e79febaea35ee2c39a5f23f928eee241c
pmb <pmb@heise.de>
2020-04-19 15:17:29 +0200

    fix: Do not include the whole package.json in the build artifact

--- index.ts
+++ index.ts
@@ -1,7 +1,1 @@
 export * from './lib/embetty'
-function debugEnabled() {
-  return window.localStorage.embettyDebug === 'true'
-}
-if (debugEnabled()) {
-  console.log(`embetty-version: ${require('./package.json').version}`)
-}
