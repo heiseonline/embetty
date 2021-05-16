e4750306db331d99cebb2054fd0faf70d1cdb1ca
pmb <pmb@heise.de>
2021-05-16 23:26:09 +0200

    test: skip flaky tests

--- test/facebook.js
+++ test/facebook.js
@@ -2,14 +2,16 @@ import { createFacebookVideo } from './lib/util'
 import assert from 'assert'
 
 describe('Facebook Video', () => {
-  it('should provide no poster image', async () => {
+  // flaky
+  it.skip('should provide no poster image', async () => {
     const { query } = await createFacebookVideo('10156049485672318', {
       'poster-image': '',
     })
     assert.ok(!query('img'))
   })
 
-  it('should provide the given poster image', async () => {
+  // flaky
+  it.skip('should provide the given poster image', async () => {
     const posterImage = 'http://www.test.de/facebook'
     const { query } = await createFacebookVideo('10156049485672318', {
       'poster-image': posterImage,
