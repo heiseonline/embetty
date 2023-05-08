e84df2ecdc4893b2f0d9c92bf7869a42c4606806
pmb <pmb@heise.de>
2023-05-08 15:50:41 +0200

    chore: changeset

---
+++ .changeset/mean-radios-flow.md
@@ -0,0 +1,5 @@
+---
+'@embetty/base': patch
+---
+
+Properly use EMBETTY_CACHE env var
--- libs/base/src/cache/redis.ts
+++ libs/base/src/cache/redis.ts
@@ -3,7 +3,11 @@ import { deserialize, serialize } from 'v8'
 import { CacheEntry, EmbettyCache } from './cache'
 
 export class Redis implements EmbettyCache {
-  #redis = new IORedis()
+  #redis: IORedis
+
+  constructor(url?: string) {
+    this.#redis = url ? new IORedis(url) : new IORedis()
+  }
 
   async get<T>(key: string): Promise<CacheEntry<T> | undefined> {
     const response = await this.#redis.getBuffer(key)
