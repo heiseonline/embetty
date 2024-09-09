0db1d8f118536ea53fb22bb5dc09832a68cdc7c4
pmb <pmb@heise.de>
2024-09-09 13:19:40 +0200

    fix: mention embetty server version on bootstrap

--- embetty-server/src/server.ts
+++ embetty-server/src/server.ts
@@ -1,7 +1,9 @@
+/* eslint-disable no-console */
+import { version } from '../package.json'
 import { app } from './app'
 
 // eslint-disable-next-line @typescript-eslint/no-magic-numbers
 const server = app.listen(process.env.PORT || 3000)
 
-// eslint-disable-next-line no-console
+console.log('🐙 Embetty Server', version)
 console.log('Listening on port', server.address())
